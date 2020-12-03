711d44b3a4a119c9d4d2deff8cdddefc4b20ab79
Oleg Panikarovskiy <oleg.pnk@gmail.com>
2020-12-03 22:51:07 +0300

    add note-item

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -4,12 +4,13 @@ import { NotesRoutingModule } from 'src/app/notes/notes-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ListComponent } from './components/list/list.component';
 import { MainComponent } from './components/main/main.component';
+import { NoteItemComponent } from './components/note-item/note-item.component';
 
 
 @NgModule({
   declarations: [
     ListComponent, //
-    MainComponent,
+    MainComponent, NoteItemComponent,
   ],
   imports: [
     CommonModule, //
